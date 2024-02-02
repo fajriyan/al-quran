d972e554abb2faa1a59770082fb562fe0c6d49e8
fajriyan <fajriyan20@gmail.com>
2024-02-02 20:55:58 +0700

    update [1.7.0] delete testing and improve seo

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("main"));
@@ -13,4 +12,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-reportWebVitals();
--- src/page/Changelog.jsx
+++ src/page/Changelog.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 
 const Changelog = () => {
@@ -61,77 +62,89 @@ const Changelog = () => {
   ];
 
   return (
-    <div className="container mx-auto py-5 px-3 md:px-0">
-      <div className="flex items-center gap-3">
-        <Link to={"/"}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            fill="currentColor"
-            className="bi bi-arrow-left-short hover:scale-110"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"
-            />
-            <path
-              fill-rule="evenodd"
-              d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"
-            />
-          </svg>
-        </Link>
-        <h1 className="font-bold text-lg">Changelog Al Quran Digital</h1>
-      </div>
+    <>
+      {/* Helmet Start  */}
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Changelog Website Al-Quran</title>
+        <link rel="canonical" href={"https://al-quran.pages.dev/changelog"} />
+      </Helmet>
 
-      <div className="mt-3">
-        <div class="overflow-x-auto">
-          <table class="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-            <thead class="text-left">
-              <tr>
-                <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                  ID
-                </th>
-                <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                  Author
-                </th>
-                <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                  Date
-                </th>
-                <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                  Version
-                </th>
-                <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                  Description
-                </th>
-              </tr>
-            </thead>
+      {/* Helmet End  */}
+      <div className="container mx-auto py-5 px-3 md:px-0">
+        <div className="flex items-center gap-3">
+          <Link to={"/"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="bi bi-arrow-left-short hover:scale-110"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"
+              />
+            </svg>
+          </Link>
+          <h1 className="font-bold text-lg">Changelog Al Quran Digital</h1>
+        </div>
 
-            <tbody class="divide-y divide-gray-200">
-              {dataChangelog.map((data, i) => (
-                <tr key={Math.random(19) + data.id}>
-                  <td class="px-4 py-2 font-medium text-gray-900">{data.id}</td>
-                  <td class="px-4 py-2 text-gray-700">{data.author}</td>
-                  <td class="px-4 py-2 text-gray-700">{data.date}</td>
-                  <td class="px-4 py-2 text-gray-700">{data.version}</td>
-                  <td class="px-4 py-2 text-gray-700">{data.description}</td>
+        <div className="mt-3">
+          <div class="overflow-x-auto">
+            <table class="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+              <thead class="text-left">
+                <tr>
+                  <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    ID
+                  </th>
+                  <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    Author
+                  </th>
+                  <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    Date
+                  </th>
+                  <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    Version
+                  </th>
+                  <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    Description
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
 
-        <div className="mt-6">
-          <a
-            href="https://github.com/fajriyan/al-quran/issues/new"
-            className="px-5 py-2 rounded-md bg-slate-800 hover:bg-slate-900 font-medium text-white "
-          >
-            Tambah Fitur
-          </a>
+              <tbody class="divide-y divide-gray-200">
+                {dataChangelog.map((data, i) => (
+                  <tr key={Math.random(19) + data.id}>
+                    <td class="px-4 py-2 font-medium text-gray-900">
+                      {data.id}
+                    </td>
+                    <td class="px-4 py-2 text-gray-700">{data.author}</td>
+                    <td class="px-4 py-2 text-gray-700">{data.date}</td>
+                    <td class="px-4 py-2 text-gray-700">{data.version}</td>
+                    <td class="px-4 py-2 text-gray-700">{data.description}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          <div className="mt-6">
+            <a
+              href="https://github.com/fajriyan/al-quran/issues/new"
+              className="px-5 py-2 rounded-md bg-slate-800 hover:bg-slate-900 font-medium text-white "
+            >
+              Tambah Fitur
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
